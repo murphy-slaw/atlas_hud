4c4fa96bf49c954a1d29e4c1972fdce3e5d6117b
Kevin Murphy <kevin@eorbit.net>
2025-11-26 21:25:22 -0800

    Oops, those config fields should not be final.

--- src/main/java/net/funkpla/atlas_hud/AtlasHudConfig.java
+++ src/main/java/net/funkpla/atlas_hud/AtlasHudConfig.java
@@ -9,33 +9,33 @@ public class AtlasHudConfig implements ConfigData {
 
     @ConfigEntry.Category("Display")
     @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
-    public final CompassDisplayRule DisplayRule = CompassDisplayRule.COMPASS_HELD;
+    public CompassDisplayRule DisplayRule = CompassDisplayRule.COMPASS_HELD;
     @ConfigEntry.Category("Display")
-    public final boolean ShowDirections = true;
+    public boolean ShowDirections = true;
     @ConfigEntry.Category("Display")
-    public final boolean ShowMarkers = true;
+    public boolean ShowMarkers = true;
     @ConfigEntry.Category("Display")
-    public final boolean DrawBackground = true;
+    public boolean DrawBackground = true;
     @ConfigEntry.Category("Display")
     @ConfigEntry.BoundedDiscrete(min = 30, max = 360)
-    public final int CompassArc = 240;
+    public int CompassArc = 240;
     @ConfigEntry.Category("Display")
     @ConfigEntry.BoundedDiscrete(min = 50, max = 100)
-    public final int CompassWidth = 90;
+    public int CompassWidth = 90;
     @ConfigEntry.Category("Display")
     @ConfigEntry.BoundedDiscrete(min = 1, max = 100)
-    public final int CompassOpacity = 100;
+    public int CompassOpacity = 100;
     @ConfigEntry.Category("Display")
-    public final int CompassOffset = 4;
+    public int CompassOffset = 4;
     @ConfigEntry.Category("Display")
     @ConfigEntry.BoundedDiscrete(min = 35, max = 100)
-    public final int MarkerScale = 100;
+    public int MarkerScale = 100;
     @ConfigEntry.Category("Display")
     @ConfigEntry.ColorPicker
-    public final int CompassTextColor = 0xffffff;
+    public int CompassTextColor = 0xffffff;
     @ConfigEntry.Category("Display")
     @ConfigEntry.ColorPicker
-    public final int CompassBackgroundColor = 0x000000;
+    public int CompassBackgroundColor = 0x000000;
 
     public enum CompassDisplayRule {
         ALWAYS, COMPASS_HELD, COMPASS_HOTBAR, COMPASS_INVENTORY
